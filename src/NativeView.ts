1dca6627bbe0db8b74cf6e28747d8f2573a24b86
Mykola Kodrul <mkodrul@snapchat.com>
2024-01-30 14:47:27 -0800

    remove functionality JS side

--- src/NativeView.ts
+++ src/NativeView.ts
@@ -2,17 +2,9 @@ import { type ViewProps } from 'react-native';
 import { getNativeViewManager } from './verifyNativeModule';
 import type { Rect } from './CameraKitContextModule';
 
-export interface CropConfig {
-    aspectRatioNumerator: number;
-    aspectRatioDenominator: number;
-}
-
 export interface CameraOptions {
     position: 'front' | 'back';
-    ratio: 'RATIO_16_9' | 'RATIO_4_3';
-    mirrorFramesHorizontally: boolean;
     mirrorFramesVertically: boolean;
-    crop: CropConfig;
     cameraPosition: 'front' | 'back';
     safeRenderArea: Rect;
 }
