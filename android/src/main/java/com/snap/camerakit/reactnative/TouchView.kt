9b5700d701a4369e3d7c2d1e8938116ce69eb339
Mykola Kodrul <mkodrul@snapchat.com>
2024-02-02 10:54:02 -0800

    log touch event

--- android/src/main/java/com/snap/camerakit/reactnative/TouchView.kt
+++ android/src/main/java/com/snap/camerakit/reactnative/TouchView.kt
@@ -1,6 +1,8 @@
 package com.snap.camerakit.reactnative
 
 import android.content.Context
+import android.util.Log
+import android.view.MotionEvent
 import android.view.ViewStub
 import android.widget.FrameLayout
 
@@ -9,5 +11,10 @@ class TouchView(context: Context) : FrameLayout(context) {
     init {
         inflate(context, R.layout.camera_kit_touch, this)
         this.textureView = findViewById(R.id.ck_touch_view)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.i("myk", "touch event!!!!")
+        return super.onTouchEvent(event)
     }
 }
